a5656d94ce7666df73df13fede89ac7fbaea4d5a
Rannyeri Baptist <rannyerirbatista@gmail.com>
2020-04-18 13:48:43 -0300

    devise screens finished

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def show_flash_messages
+    if flash.any?
+      flash.each do |f|
+        return ("<div class='flash flash--#{f[0]} animated fadeInDown'>#{f[1]}</div>").html_safe
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+
   resources :products
-  devise_for :users
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 end
