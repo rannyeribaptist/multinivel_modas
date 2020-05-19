e4ff9b20186fc8f8d2914b3ce347dd9cef974d35
Rannyeri Baptist <rannyerirbatista@gmail.com>
2020-05-19 01:04:40 -0300

    fixed issue with finish registration form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
+  before_action :validate_registration_of_current_user
+
+  private
+
+  def validate_registration_of_current_user
+    if current_user.present?
+      if not current_user.address.present?
+        redirect_to finish_registration_path if not request.path == "/concluir_cadastro"
+      end
+    end
+  end
 end
