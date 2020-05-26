c25a3666a9015c784e4d88a55785833a5ab2485e
Rannyeri Baptist <rannyerirbatista@gmail.com>
2020-05-26 17:03:11 -0300

    translations in pt-br

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module MultinivelModas
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # translations
+    config.i18n.default_locale = "pt-BR"
   end
 end
