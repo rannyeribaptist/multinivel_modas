77763fb41ede21f18ead1712a371af0cab43d643
Rannyeri Baptist <rannyerirbatista@gmail.com>
2020-06-22 06:57:14 -0300

    removed belongs_to line in create products category migration

--- db/migrate/20200312164243_create_product_categories.rb
+++ db/migrate/20200312164243_create_product_categories.rb
@@ -2,7 +2,7 @@ class CreateProductCategories < ActiveRecord::Migration[6.0]
   def change
     create_table :product_categories do |t|
       t.string :name
-      t.belongs_to :product, null: false, foreign_key: true
+      # t.belongs_to :product, null: false, foreign_key: true
 
       t.timestamps
     end
