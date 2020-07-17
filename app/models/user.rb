119ba0bc0415380cd344f26e85994e5fc652e545
Rannyeri Baptist <rannyerirbatista@gmail.com>
2020-07-17 05:55:45 -0300

    solved issue with user registration invitation code

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,23 +19,12 @@ class User < ApplicationRecord
 
   serialize :invited_ids, Array
 
-  # validates_presence_of :name
-  # validates_presence_of :role
-  # validates_presence_of :graduation
-  # validates_presence_of :level
-
   validates :role, acceptance: { accept: ["seller", "consultant", "admin", "client", "franchise"] }
   validates :graduation, acceptance: { accept: ["sênior", "bronze", "prata", "ouro", "diamante", "imperial"] }
 
-  validates_uniqueness_of :invitation_token
+  validates_uniqueness_of :invitation_token, only: :update
   validates_uniqueness_of :social_security_number, only: :update, :unless => lambda { self.tax_number.present? }
   validates_uniqueness_of :tax_number, only: :update, :unless => lambda { self.social_security_number.present? }
-
-  # validate :social_id
-  # validate :user_role
-  # validate :address_presence
-  # validate :bank_account_information_presence
-  # validate :credit_information_presence
 
   accepts_nested_attributes_for :address, allow_destroy: :false, reject_if: :all_blank
   accepts_nested_attributes_for :bank_account_information, allow_destroy: :false, reject_if: :all_blank
