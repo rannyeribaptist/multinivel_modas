ca45b6adf318525eba5fb6f9f11e254cbaafc0cd
Rannyeri Baptist <rannyerirbatista@gmail.com>
2020-10-15 03:47:20 -0300

    finished revising functionality for finance user

--- app/controllers/withdraws_controller.rb
+++ app/controllers/withdraws_controller.rb
@@ -22,7 +22,7 @@ class WithdrawsController < ApplicationController
   end
 
   def authorize_withdraws
-    @withdraws = Withdraw.all if current_user.role == "admin"
+    @withdraws = Withdraw.all if current_user.role == "finance"
   end
 
   # POST /withdraws
